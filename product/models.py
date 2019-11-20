5b310a8431a1dc7fd3899054a3e520380b3d393a
icaro.souza <icarosoares@ciandt.com>
2019-11-19 23:01:59 -0300

    Add Product model

--- product/models.py
+++ product/models.py
@@ -1,3 +1,10 @@
+"""The product model"""
 from django.db import models
 
-# Create your models here.
+
+class Client(models.Model):
+    brand = models.CharField(max_length=150)
+    title = models.CharField(max_length=70)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    image = models.URLField(max_length=200, null=True, blank=True)
+    reviewScore = models.FloatField(default=0.0)
